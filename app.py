32504701b062138cbf5435ea9af9814e595d0ad8
skai2105 <shahirkhan3541@gmail.com>
2024-11-21 18:42:51 -0700

    added templates

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<html><body><h1>SKAI Solutions</h1><div><p>Multi-Cloud Architecture | AI & Machine Learning | DevOps Consulting</p><ul><li><a href='#'>Home</a></li><li><a href='#'>About</a></li><li><a href='#'>Services</a></li><li><a href='#'>Products</a></li><li><a href='#'>Contact</a></li></ul></div><div><p>&copy; 2024 CloudTech Solutions</p></div></body></html>\n"
+    return render_template('home.html')
 
