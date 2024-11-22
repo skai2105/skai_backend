a85b2a6d7f41fe43266b513bbbf950f473024054
skai2105 <shahirkhan3541@gmail.com>
2024-11-21 18:17:07 -0700

    updated code for actual website

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<html><body><h1>Hello from my App!</h1></body></html>\n"
+    return "<html><body><h1>SKAI Solutions</h1><div><p>Multi-Cloud Architecture | AI & Machine Learning | DevOps Consulting</p><ul><li><a href='#'>Home</a></li><li><a href='#'>About</a></li><li><a href='#'>Services</a></li><li><a href='#'>Products</a></li><li><a href='#'>Contact</a></li></ul></div><div><p>&copy; 2024 CloudTech Solutions</p></div></body></html>\n"
+
